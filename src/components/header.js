d005641399df90181d8f0ab59bfb8b0cf554b900
tiger <thisisstreamliner@gmail.com>
2022-12-14 00:42:19 +0200

    Add State

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 
 class Header extends Component {
 
+    state = {
+        title: 'The keywords are:',
+        keywords: '',
+        count: 0,
+    }
+
     inputChange = (event) => {
-        console.log(event.target.value)
+        this.setState({
+            keywords: event.target.value
+        })
+    }
+
+    addOne() {
+        this.setState((state, props) => ({ 
+            count: state.count + 1
+        }))
     }
 
     render() {
@@ -11,6 +25,12 @@ class Header extends Component {
             <header>
                 <div className="logo" onClick={() => console.log('I was clicked')}>Logo</div>
                 <input onChange={this.inputChange}/>
+                <br/>
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
+                <br/>
+                <div>{this.state.count}</div>
+                <button onClick={() => this.addOne()}>Add One</button>
             </header>
         )
     }
