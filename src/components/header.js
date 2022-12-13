b06cdddc14a1cb8c7e445de79ece4b8818aa57ee
tiger <thisisstreamliner@gmail.com>
2022-12-14 00:24:42 +0200

    Add Events

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 class Header extends Component {
 
+    inputChange = (event) => {
+        console.log(event.target.value)
+    }
+
     render() {
         return (
             <header>
-                <div className="logo">Logo</div>
-                <input/>
+                <div className="logo" onClick={() => console.log('I was clicked')}>Logo</div>
+                <input onChange={this.inputChange}/>
             </header>
         )
     }
