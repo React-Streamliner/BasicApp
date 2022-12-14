b945518879c3dbf37e003234c6702d0eff43bb7b
tiger <thisisstreamliner@gmail.com>
2022-12-15 00:00:20 +0200

    Add Dynamic Styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 class Header extends Component {
 
     state = {
+        active: false,
         title: 'The keywords are:',
         keywords: '',
         count: 0,
     }
 
     inputChange = (event) => {
+        const value = event.target.value === '' ? false : true
         this.setState({
-            keywords: event.target.value
+            keywords: event.target.value,
+            active: value,
         })
     }
 
@@ -22,7 +25,8 @@ class Header extends Component {
 
     render() {
         return (
-            <header>
+            //<header style={{background: `${this.state.active ? 'red' : 'blue'}`}}>
+            <header className={this.state.active ? 'active' : 'not-active'}>
                 <div className="logo" onClick={() => console.log('I was clicked')}>Code News:</div>
                 <input onChange={this.inputChange}/>
             </header>
