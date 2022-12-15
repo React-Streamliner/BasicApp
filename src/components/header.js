4c86bb8c6c7a86fdf0016c835d92c32cf2200845
tiger <thisisstreamliner@gmail.com>
2022-12-15 18:00:11 +0200

    Add Filter For News

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,13 @@
 import React, { Component } from "react";
 
-class Header extends Component {
-
-    state = {
-        active: false,
-        title: 'The keywords are:',
-        keywords: '',
-        count: 0,
-    }
-
-    inputChange = (event) => {
-        const value = event.target.value === '' ? false : true
-        this.setState({
-            keywords: event.target.value,
-            active: value,
-        })
-    }
-
-    addOne() {
-        this.setState((state, props) => ({ 
-            count: state.count + 1
-        }))
-    }
-
-    render() {
-        return (
-            //<header style={{background: `${this.state.active ? 'red' : 'blue'}`}}>
-            <header className={this.state.active ? 'active' : 'not-active'}>
-                <div className="logo" onClick={() => console.log('I was clicked')}>Code News:</div>
-                <input onChange={this.inputChange}/>
-            </header>
-        )
-    }
+const Header = (props) => {
+    return (
+        <header className={props.active ? 'active' : 'not-active'}>
+            <div className="logo" onClick={() => console.log('I was clicked')}>Code News:</div>
+            <input onChange={props.keywords}/>
+            <button onClick={props.changeColor}>Change it</button>
+        </header>
+    )
 }
 
 
--- src/index.js
+++ src/index.js
@@ -11,20 +11,43 @@ import Footer from "./components/footer";
 class App extends Component {
 
     state = {
+        active: false,
         news: JSON,
+        filtered: [],
         footerText: 'I am a happy footer.',
+    }
+
+    getKeywords = (event) => {
+        let keywords = event.target.value
+        let filtered = this.state.news.filter((item) => {
+            return item.title.indexOf(keywords) > -1
+        })
+        this.setState({
+            filtered: filtered
+        })
+    }
+
+    changeColor = () => {
+        this.setState({
+            active: this.state.active ? false : true
+        })
     }
     
     render() {
+        const {filtered, news, active, footerText} = this.state
         return (
             <div>
-                <Header/>
+                <Header 
+                    active={active}
+                    changeColor={this.changeColor}
+                    keywords={this.getKeywords}
+                />
                 <NewsList
-                    news={this.state.news}>
+                    news={filtered.length === 0 ? news : filtered}>
                     <br/>
                     <div>I am children!</div>
                 </NewsList>
-                <Footer footerText={this.state.footerText}/>
+                <Footer footerText={footerText}/>
             </div>
         )
     }
